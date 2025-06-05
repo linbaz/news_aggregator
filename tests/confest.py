78f7c06cd378bed05fc2d1cca65aeb7ddba5b1b1
Олександр Куценко <linbaz2613459870@gmail.com>
2025-06-05 09:29:47 +0300

    test

--- tests/confest.py
+++ tests/confest.py
@@ -1,3 +1,6 @@
-import sys, os
-root = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+import sys
+import os
+
+# Додаємо шлях до config.py (на один рівень вище кореня проєкту)
+root = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 sys.path.insert(0, root)
